2d9d06eee7be05801f71e6dc05498533976e31a2
mjkahan <hello@kahandatasolutions.com>
2022-07-21 21:41:39 -0400

    add orders mart

--- models/staging/payment_app/stg_payment_app__transactions.sql
+++ models/staging/payment_app/stg_payment_app__transactions.sql
@@ -10,8 +10,15 @@ final as (
 
     select
         id as transaction_id,
-        date as created_at,
-        payload
+        payload,
+        payload:amount::numeric(18,2) as amount_in_usd,
+        payload:cost_per::numeric(18,2) as cost_per_unit_in_usd,
+        payload:order_id::integer as order_id,
+        payload:product_id::integer as product_id,
+        payload:quantity::integer as quantity, 
+        payload:tax::numeric(18,2) as tax_in_usd,
+        payload:total_charged::numeric(18,2) as total_charged_in_usd,
+        date as created_at
 
     from transactions
 
